25b35711a9bddce70b18e63e6d6fb58ab36309af
Remi <noruto2021@gmail.com>
2024-10-07 19:20:38 +0300

    added pan animation on modals

---
+++ src/app/providers/with-gestureHandler.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FC } from 'react'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+
+export const withGestureHandler = (Component: FC): FC => {
+  return (props) => {
+    return (
+      <GestureHandlerRootView>
+        <Component {...props} />
+      </GestureHandlerRootView>
+    )
+  }
+}
