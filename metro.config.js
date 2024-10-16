fbf472e6bb42d30ff78fe2f02a52bf050ce7c494
Remi <noruto2021@gmail.com>
2024-10-16 17:26:04 +0300

    changes

--- metro.config.js
+++ metro.config.js
@@ -15,28 +15,5 @@ module.exports = (() => {
     sourceExts: [...resolver.sourceExts, 'svg'],
   }
 
-  const ALIASES = {
-    '@shared/*': './src/shared/*',
-    '@widgets/*': './src/widgets/*',
-    '@features/*': './src/features/*',
-    '@pages/*': './src/pages/*',
-    '@icons/*': './assets/icons/*',
-    '@fonts/*': './assets/fonts/*',
-    '@modules/*': './modules/*',
-  }
-
-  config.resolver.resolveRequest = (context, moduleName, platform) => {
-    if (platform === 'web') {
-      // The alias will only be used when bundling for the web.
-      return context.resolveRequest(
-        context,
-        ALIASES[moduleName] ?? moduleName,
-        platform,
-      )
-    }
-    // Ensure you call the default resolver.
-    return context.resolveRequest(context, moduleName, platform)
-  }
-
   return config
 })()
