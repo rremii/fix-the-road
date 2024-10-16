46ee5908e6c42001d4718549969b228b325ce30b
Remi <noruto2021@gmail.com>
2024-10-16 19:13:56 +0300

    fix

--- src/app/navigation/index.tsx
+++ src/app/navigation/index.tsx
@@ -4,5 +4,6 @@ import DesktopNavigation from './desktop/Root'
 import React from 'react'
 
 export const RootNavigation = () => {
-  return Platform.OS === 'web' ? <DesktopNavigation /> : <MobileNavigation />
+  // return Platform.OS === 'web' ? <DesktopNavigation /> : <MobileNavigation />
+  return <DesktopNavigation />
 }
